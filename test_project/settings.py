94cdbe6a21e1548ddc3d4df752037c5acb6ba2bf
Akles <yak.3u@mail.ru>
2022-02-19 21:57:19 +0700

    initial commit

--- test_project/settings.py
+++ test_project/settings.py
@@ -39,12 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig', # новое приложение
+    'pages.apps.PagesConfig', # новое приложение   'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
